33d8305c8f3eecdd1a72ed2921206ab167e15b49
Seriusman Waruwu <61109185+seriusman0@users.noreply.github.com>
2026-01-11 07:54:26 +0700

    create csrf

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -17,6 +17,7 @@
     @routes
     @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
     @inertiaHead
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 
 <body class="font-sans antialiased bg-gray-200 dark:bg-gray-900">
